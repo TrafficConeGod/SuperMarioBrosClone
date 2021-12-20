a616a984f8cb6f430c364613c932456a168f732f
TrafficConeGod <TrafficConeGod@github.com>
2021-12-20 18:18:44 -0500

    add correct resolution

--- Assets/Classes/Nodes/Player.cs
+++ Assets/Classes/Nodes/Player.cs
@@ -2,7 +2,7 @@ using Godot;
 using System;
 
 /// <Summary> This node has a lifetime for the entire existence of the game stage system being played </Summary>
-public class Player : Node {
+public class Player : Node2D {
     public override void _Ready() {
         var stage = new Stage();
         stage.Construct(new DataStream("Assets/Stages/MainStage.stg"));
