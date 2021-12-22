c419ae3059cf071471ed920a952aef30060e2c27
TrafficConeGod <TrafficConeGod@github.com>
2021-12-22 10:59:16 -0500

    add tilemap

--- Assets/Classes/Nodes/Player.cs
+++ Assets/Classes/Nodes/Player.cs
@@ -4,6 +4,7 @@ using System;
 /// <Summary> This node has a lifetime for the entire existence of the game stage system being played </Summary>
 public class Player : Node2D {
     public override void _Ready() {
+        OS.WindowSize = new Vector2(768, 720); // hacky way to set the window size
         var stage = new Stage();
         stage.Construct(new DataStream("Assets/Stages/MainStage.stg"));
         AddChild(stage);
---
+++ Assets/Scenes/Actors/Tilemap/Tilemap.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+[Actor(Id = 3, Scene = "res://Assets/Scenes/Actors/Tilemap/Tilemap.tscn")]
+public class Tilemap : Actor {
+    public override void _Ready() {
+        
+    }
+}
